a47a48f4f9cbe26f76b54c2599376c9a521ce082
OlegEfrem <oleg.efrem@gmail.com>
2022-09-16 02:10:00 +0100

    Add external integration with product info service

---
+++ src/it/scala/com/shopping/cart/integration/DefaultProductInfoServiceTest.scala
@@ -0,0 +1,23 @@
+package com.shopping.cart.integration
+
+import com.shopping.cart.domain.model.ProductName
+import munit.CatsEffectSuite
+
+class DefaultProductInfoServiceTest extends CatsEffectSuite {
+  private val service = ProductInfoService()
+
+  private val expectedPrices: Map[ProductName, BigDecimal] = Map(
+    ProductName.Cheerios -> 8.43,
+    ProductName.Cornflakes -> 2.52,
+    ProductName.Frosties -> 4.99,
+    ProductName.Shreddies -> 4.68,
+    ProductName.Weetabix -> 9.98
+  )
+
+  ProductName.values.foreach { productName =>
+    val expectedPrice: BigDecimal = expectedPrices(productName)
+    test(s"should return $expectedPrice for $productName") {
+      service.priceFor(productName).map(price => assertEquals(price.amount, expectedPrice))
+    }
+  }
+}
---
+++ src/main/scala/com/shopping/cart/integration/ProductInfoService.scala
@@ -0,0 +1,40 @@
+package com.shopping.cart.integration
+
+import cats.effect.IO
+import com.shopping.cart.domain.model.{Money, ProductName}
+import okhttp3.{OkHttpClient, Request}
+
+trait ProductInfoService {
+  def priceFor(product: ProductName): IO[Money]
+}
+
+object ProductInfoService {
+  def apply(): ProductInfoService = new DefaultProductInfoService()
+}
+
+class DefaultProductInfoService extends ProductInfoService {
+
+  import io.circe.generic.auto._
+  import io.circe.parser._
+
+  case class ProductInfo(title: String, price: BigDecimal)
+
+  private val client = new OkHttpClient()
+
+  override def priceFor(product: ProductName): IO[Money] = {
+    for {
+      response <- getProductInfo(product)
+      productInfo: ProductInfo <- IO.fromEither(decode[ProductInfo](response))
+    } yield Money(productInfo.price)
+  }
+
+  type ResponsePayload = String
+
+  private def getProductInfo(product: ProductName): IO[ResponsePayload] = IO {
+    val url = s"https://raw.githubusercontent.com/mattjanks16/shopping-cart-test-data/main/${product.entryName.toLowerCase}.json"
+    val request = new Request.Builder()
+      .url(url)
+      .build()
+    client.newCall(request).execute().body().string()
+  }
+}
